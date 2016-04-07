715cf2dc4154e953a0985b8865c903e63d66984f
tedcurrent <tetsiiedm@gmail.com>
2016-04-07 14:25:55 +0200

    refactor

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,7 +2,8 @@
 
 var express = require("express");
 var router = express.Router();
-var ScoreController = require("../controllers/scoreController");
+var LolApiController = require("../controllers/lolApiController");
+var Util = require("../utils/util");
 
 router.get("/getGames/:name/:region", function(req, res, next) {
 	var summonerInfo = {
@@ -10,12 +11,34 @@ router.get("/getGames/:name/:region", function(req, res, next) {
 			region: req.params.region
 	};
 
-	ScoreController.newScores(summonerInfo, function(err, result) {
+	LolApiController.getRecentGamesWithSummoner(summonerInfo, function(err, result) {
 		if (err) {
 			return next(err);
 		}
+
 		res.json(result);
 	});
 });
 
+router.post("/checkNewHighScore", function(req, res, next) {
+	res.json({
+		hello: "I will eventually check for a new highscore",
+		req: req.body
+	});
+});
+
 module.exports = router;
+
+
+// Eventual return
+// {
+// 	summoner: {},
+// 	highScore: {},
+// 	recentScores: {},
+// 	newHighScore: boolean
+// }
+
+// Flow:
+// Front calls: get Games
+// Front calls: check for new Highscore after get Games
+// This way front is responsible for the flow and server just does stuff
