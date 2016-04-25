211327d1b67c84227f6a0a292541d99840551a32
tedcurrent <tetsiiedm@gmail.com>
2016-04-25 17:03:23 +0300

    meta and favicon

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.set("views", path.join(__dirname, "/server/views"));
 app.set("view engine", "jade");
 
 // uncomment after placing your favicon in /dist
-//app.use(favicon(path.join(__dirname, "dist", "favicon.ico")));
+app.use(favicon(path.join(__dirname, "dist/icons", "favicon.ico")));
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
