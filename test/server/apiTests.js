fd9fbff87370fb6ca9b59935875bacbf307d2eaf
tedcurrent <tetsiiedm@gmail.com>
2016-05-01 12:47:48 +0300

    re-did api tests

--- test/server/apiTests.js
+++ test/server/apiTests.js
@@ -1,43 +1,100 @@
 "use strict";
 var test = require("tape");
-var LolApiController = require("../../server/controllers/lolApiController");
-var HighScoreController = require("../../server/controllers/highScoreController");
+var request = require("supertest");
 
-var _testSummoner = {
-	id: 51520537,
-	name: "tetsii",
-	profileIconId: 28,
-	region: "euw",
-	level: 30
-}
+var _config = {
+	testSummoner: {
+		id: 51520537,
+		name: "tetsii",
+		profileIconId: 28,
+		region: "euw",
+		level: 30
+	},
 
-test("Test summoner fetch with name and region", function(t) {
-	LolApiController.getSummoner(_testSummoner, function(err, result) {
-		t.equal(err, null, "Error should be null");
-		t.equal(result.name, _testSummoner.name, "Names should be equal");
-		t.end();
-	});
+	testGame: {
+	    gameId: 2337165086,
+	    gameMode: "CLASSIC",
+	    champion: "Morgana",
+	    gameDate: 1444756345019,
+	    dmgDealt: 9178
+  	},
+
+	api: {
+		path: "localhost:3001/api"
+	}
+};
+
+test("GET /getSummoner/:id/:region", function(t) {
+	request(_config.api.path)
+		.get("/getSummoner/" + _config.testSummoner.name + "/" + _config.testSummoner.region)
+		.set('Accept', 'application/json')
+		.expect('Content-Type', /json/)
+		.expect(200)
+		.end(function (err, res) {
+			t.error(err);
+			var result = JSON.parse(res.text);
+			t.equal(result.name, _config.testSummoner.name, "Names should be equal");
+			t.end();
+		});
 });
 
-test("Test summoner fetch with id and region", function(t) {
-	LolApiController.getSummonerWithId(_testSummoner.id, _testSummoner.region, function(err, result) {
-		t.equal(err, null, "Error should be null");
-		t.equal(result.id, _testSummoner.id, "ID's should be equal");
-		t.end();
-	});
+test("GET /getSummonerWithId/:id/:region", function(t) {
+	request(_config.api.path)
+		.get("/getSummonerWithId/" + _config.testSummoner.id + "/" + _config.testSummoner.region)
+		.expect(200)
+		.set('Accept', 'application/json')
+		.expect('Content-Type', /json/)
+		.end(function (err, res) {
+			t.error(err);
+			var result = JSON.parse(res.text);
+			t.equal(result.id, _config.testSummoner.id, "ID's should be equal");
+			t.end();
+		});
 });
 
-test("Test highScore search + db connectivity", function(t) {
-	var testReq = {
-		body: {
-			id: 51520537,
-			region: "euw"
-		}
-	};
-	
-	HighScoreController.findBySummonerId(testReq, function(err, result) {
-		t.equal(err, null, "Error should be null");
-		t.equal(result.summoner.id, testReq.body.id, "ID's should be equal");
-		t.end();
-	});
+test("GET /getGames/:summonerId/:region", function(t) {
+	request(_config.api.path)
+		.get("/getGames/" + _config.testSummoner.id + "/" + _config.testSummoner.region)
+		.expect(200)
+		.set('Accept', 'application/json')
+		.expect('Content-Type', /json/)
+		.end(function (err, res) {
+			t.error(err);
+			var result = JSON.parse(res.text);
+			t.equal(result.length, 10, "Should return 10 games");
+			t.end(); 
+		});
+});
+
+test("GET /getRegionalScores", function(t) {
+	request(_config.api.path)
+		.get("/getRegionalScores")
+		.expect(200)
+		.set('Accept', 'application/json')
+		.expect('Content-Type', /json/)
+		.end(function (err, res) {
+			t.error(err);
+			var result = JSON.parse(res.text);
+			t.ok(result.length, "Should contain games");
+			t.end();
+		});
+});
+
+test("POST /saveHighScore", function(t) {
+	var query = _config.testSummoner;
+	query.topGame = _config.testGame;
+
+	request(_config.api.path)
+		.post("/saveHighScore")
+		.send(query)
+		.expect(200)
+		.set('Accept', 'application/json')
+		.expect('Content-Type', /json/)
+		.end(function (err, res) {
+			t.error(err);
+			var result = JSON.parse(res.text);
+			t.equal(result.highScore.summoner.summonerId, query.id, "Should find summoner with ID");
+			t.ok(result.highScore.game, "Should find a highScore");
+			t.end();
+		});
 });
