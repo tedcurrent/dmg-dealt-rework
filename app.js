82531540aa4ce4c85a0e39b4b9edb049eb40a225
tedcurrent <tetsiiedm@gmail.com>
2016-05-05 20:00:41 +0300

    ratelimiter

--- app.js
+++ app.js
@@ -6,12 +6,21 @@ var favicon = require("serve-favicon");
 var logger = require("morgan");
 var cookieParser = require("cookie-parser");
 var bodyParser = require("body-parser");
+var rateLimit = require("express-rate-limit");
 
 // routes
 var index = require("./server/routes/index");
 var api = require("./server/routes/api");
 
 var app = express();
+
+// rate limiter cfg
+var limiter = rateLimit({
+	windowMs: 30000,
+	max: 100,
+	delayAfter: 50,
+	delayMs: 1000
+});
 
 // view engine setup
 app.set("views", path.join(__dirname, "/server/views"));
@@ -24,6 +33,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "dist")));
+
+// limiters
+app.use("/api/", limiter);
 
 // route middleware
 app.use("/api", api);
