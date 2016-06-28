304a9503492c272da30e6516542c6731ec29b105
tedcurrent <tetsiiedm@gmail.com>
2016-06-28 18:18:50 +0300

    collapse effect to stats

--- src/js/components/Game/GameHeader.js
+++ src/js/components/Game/GameHeader.js
@@ -5,9 +5,13 @@ var GameBackground = require("./GameBackground");
 var GameHeaderContainer = require("./GameHeaderContainer");
 
 var GameHeader = React.createClass({
+	clickHandler: function() {
+		this.props.onClick();
+	},
+
 	render: function() {
 		return (
-			<div className={this.props.gameClass}>
+			<div className={this.props.gameClass} onClick={this.clickHandler}>
 				{this.props.children}
 				<GameHeaderContainer game={this.props.game} />
 				<GameBackground champion={this.props.game.champion} />
--- src/js/components/Game/GameWrapper.js
+++ src/js/components/Game/GameWrapper.js
@@ -6,12 +6,41 @@ var GameStats = require("./GameStats");
 
 // A wrapper for the game "header" and the stats view
 var GameWrapper = React.createClass({
+	getInitialState: function() {
+		return {
+			statsOpen: false
+		};
+	},
+
+	headerClickHandler: function() {
+		this.setState({statsOpen: !this.state.statsOpen});
+	},
+
+	showStats: function() {
+		if(this.state.statsOpen) {
+			return (
+				<GameStats stats={this.props.game.stats} />
+			);
+		}
+	},
+
+	componentWillReceiveProps: function(nextProps) {
+		if (this.state.statsOpen) {
+			this.setState({statsOpen: false});
+		}
+	},
+
 	render: function() {
 		return (
 			<div className="game-wrapper">
-				<GameHeader game={this.props.game} gameClass={this.props.gameClass}>
+				<GameHeader 
+					game={this.props.game} 
+					gameClass={this.props.gameClass}
+					onClick={this.headerClickHandler}
+				>
 					{this.props.children}
 				</GameHeader>
+				{this.showStats()}
 			</div>
 		);
 	}
