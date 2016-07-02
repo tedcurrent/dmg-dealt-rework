45d5f664892f7d08deed73ab3dece645fbaba8c6
tedcurrent <tetsiiedm@gmail.com>
2016-07-02 10:30:54 +0300

    restructure

--- src/js/routes/routes.js
+++ src/js/routes/routes.js
@@ -4,11 +4,11 @@ var React = require("react");
 var ReactRouter = require("react-router");
 var IndexRoute = ReactRouter.IndexRoute;
 var Route = ReactRouter.Route;
-var App = require("../app");
+var App = require("../components/App/");
 var Splash = require("../components/Splash/");
 var Personal = require("../components/PersonalGames/");
 var Regions = require("../components/RegionalGames/");
-var NotFound = require("../404");
+var NotFound = require("../components/404/");
 
 // All possible routes listed here
 var routes = (
