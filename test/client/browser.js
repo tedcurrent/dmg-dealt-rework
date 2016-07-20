de21cb259926a7b8fd2f34a0fbd9ab38815f1ae6
tedcurrent <tetsiiedm@gmail.com>
2016-07-21 01:24:13 +0300

    clean up hc test
    - still unable to unmount properly

--- test/client/browser.js
+++ test/client/browser.js
@@ -2,16 +2,16 @@ import { jsdom } from "jsdom";
 
 const exposedProperties = ["window", "navigator", "document"];
 
-global.document = jsdom("<!doctype html><html><body></body></html>");
+global.document = jsdom("");
 global.window = document.defaultView;
 
 Object.keys(document.defaultView).forEach((property) => {
-	if (typeof global[property] === 'undefined') {
+	if (typeof global[property] === "undefined") {
 		exposedProperties.push(property);
 		global[property] = document.defaultView[property];
 	}
 });
 
 global.navigator = {
-	userAgent: 'node.js'
+	userAgent: "node.js"
 };
--- test/client/components/highchartsTest.js
+++ test/client/components/highchartsTest.js
@@ -7,7 +7,7 @@ import { spy } from "sinon";
 import Highcharts from "../../../src/js/components/Common/Highcharts";
 
 const mockChart = () => {
-	return {
+	const data = {
 		series: [
 			{
 				name: "Mock Series",
@@ -15,19 +15,23 @@ const mockChart = () => {
 			}
 		]
 	};
+
+	return (
+		<Highcharts
+			container={"mockChart"} 
+			options={data}
+		/>
+	);
 };
 
 test("<Highcharts />", (t) => {
 	spy(Highcharts.prototype, "componentDidMount");
-	const wrapper = mount(
-		<Highcharts 
-			container={"chart"} 
-			options={mockChart()}
-		/>
-	);
+	const wrapper = mount(mockChart());
 	t.ok(Highcharts.prototype.componentDidMount.calledOnce, "Should have called 'componentDidMount' once");
-	t.ok(wrapper.contains(<div id="chart" />), "Should have '#chart' container");
+	t.ok(wrapper.contains(<div id="mockChart" />), "Should have a '#mockChart' container");
 	t.equals(wrapper.props().options.series.length, 1, "Should have one (1) series object")
+
+	console.log(wrapper.debug());
 
 	t.end();
 });
