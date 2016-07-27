a2aea59df488d0875fd72d5b18342bd342ebeec9
tedcurrent <tetsiiedm@gmail.com>
2016-07-27 14:56:10 +0300

    add extra test to <Game />

--- test/client/components/gameTest.js
+++ test/client/components/gameTest.js
@@ -15,10 +15,13 @@ test("<Game />", (t) => {
 
 	t.ok(wrapper.contains(<div className="dummyDiv" />), "Should have 'dummyDiv' as a child");
 	t.ok(wrapper.find("Collapse").length, "Should contain a <Collapse />");
-	t.notOk(wrapper.find("GameStats").length, "Should not contain <GameStats /> on initial render");
+	t.ok(!wrapper.find("GameStats").length, "Should not contain <GameStats /> on initial render");
 
 	wrapper.find("GameHeader").simulate("click");
 	t.ok(wrapper.find("GameStats").length, "Should contain <GameStats /> after click");
+
+	wrapper.find("GameHeader").simulate("click");
+	t.ok(!wrapper.find("GameStats").length, "Should not contain <GameStats /> after second click");
 
 	t.end();
 });
