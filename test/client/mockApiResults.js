68741499b1f1dd2ecf981bd5fbbedb3397ba5408
tedcurrent <tetsiiedm@gmail.com>
2016-07-28 15:38:39 +0300

    finish personalGames tests

--- test/client/mockApiResults.js
+++ test/client/mockApiResults.js
@@ -6,7 +6,8 @@ export default function mockApiResults(overrides) {
 			id: 51520537,
 		},
 		highScore: {
-		    gameId: 2337165086,
+		    gameId: 2337165087,
+		    dmgDealt: 2,
 		    stats: {
 		    	timePlayed: 2312
 		    }
@@ -14,18 +15,21 @@ export default function mockApiResults(overrides) {
 		games:  [
 			{
 			    gameId: 2337165086,
+			    dmgDealt: 1,
 			    stats: {
 			    	timePlayed: 2312
 			    }
 		  	},
 		  	{
 			    gameId: 2337165087,
+			    dmgDealt: 2,
 			    stats: {
 			    	timePlayed: 2312
 			    }
 		  	}
 		],
-	  	errors: 0
+	  	errors: 0,
+	  	newHighScore: false
 	};
 
 	if (overrides) {
