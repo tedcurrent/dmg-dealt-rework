8160d225d46bada6a3a1cd1663579f04a71bde91
tedcurrent <tetsiiedm@gmail.com>
2017-09-19 10:18:34 +0300

    Add dynamic match counting

--- src/js/components/PersonalGames/PersonalContainer.js
+++ src/js/components/PersonalGames/PersonalContainer.js
@@ -8,9 +8,9 @@ import GameList from "./GameList";
 export default function PersonalContainer({results}) {
 	return (
 		<GamesContainer>
-			<TopGame summoner={results.summoner} topGame={results.highScore} newHs={results.newHighScore}/>
-			<h2>Last 10 Games of DMG</h2>
-			<GameList games={results.games}/>
+			<TopGame summoner={results.summoner} topGame={results.highScore} newHs={results.newHighScore} />
+			<h2>Last {results.games.length || "found"} Games of DMG</h2>
+			<GameList games={results.games} />
 		</GamesContainer>
 	);
 }
